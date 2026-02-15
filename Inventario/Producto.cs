c94ff5975ad6e34cfa4a39d3b57d38d12f5a9a67
Eliu-spec <eliotvalenciaga@gmail.com>
2026-02-15 14:03:59 -0600

    primer commit

---
+++ Inventario/Producto.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Inventario
+{
+    public class Producto
+    {
+        //Atributos
+        private string nombre;
+        private int numerodeserie;
+        private string categoria;
+        private int precio;
+        private int disponibles;
+
+
+        //Encapsulamiento
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public int NumeroDeSerie
+        {
+            get { return numerodeserie; }
+            set { numerodeserie = value; }
+        }
+        public string Categoria
+        {
+            get { return categoria; }
+            set { categoria = value; }
+        }
+        public int Precio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+        public int Disponibles
+        {
+            get { return disponibles; }
+            set { disponibles = value; }
+        }
+
+        //Constructor
+        public Producto(string nombre, int numerodeserie, string categoria, int precio, int dispobibles)
+        {
+            Nombre = nombre;
+            NumeroDeSerie = numerodeserie;
+            Categoria = categoria;
+            Precio = precio;
+            Disponibles = dispobibles;
+        }
+
+        //Metodos
+        public void Informacion()
+        {
+            Console.WriteLine($"El producto es: {Nombre}");
+            Console.WriteLine($"Su numero de serie es: {NumeroDeSerie}");
+            Console.WriteLine($"Su categoria es: {Categoria}");
+            Console.WriteLine($"Su precio indivudal es: {Precio}");
+            Console.WriteLine($"Total en el inventario: {Disponibles}");
+        }
+
+        public void PrecioConDescuento(int descuento)
+        {
+            int preciofinal = Precio - descuento;
+            {
+                Console.WriteLine($"El precio con descuento es: {preciofinal} ");
+            }
+        }
+
+        public void ValorTotalDelInventario()
+        {
+            int valortotal = precio * disponibles;
+            {
+                Console.WriteLine($"El total del inventario es : {valortotal}");
+            }
+        }
+
+        public void PreciodelEnvio(int envio)
+        {
+            int PrecioEnvio = Precio + envio;
+            {
+                Console.WriteLine($"El precio final del producto con envio será: {PrecioEnvio}");
+            }
+        }
+
+        public void PrecioconImpuesto(int impuesto)
+        {
+            int Conimpuesto = Precio + impuesto;
+            {
+                Console.WriteLine($"El precio con impuesto incluido es: {Conimpuesto}");
+            }
+        }
+    }
+}
---
+++ Inventario/Program.cs
@@ -0,0 +1,30 @@
+﻿using Inventario;
+//Nombre: Eliu Hesiel Vallecillo Amaya
+//NumerodeCuenta: 20231930134
+
+//Creacion de Productos
+Producto producto = new Producto("Sansung S21 Ultra", 101, "Tecnologia", 8000, 2);
+Producto producto1 = new Producto("Camisa Polo", 201, "Ropa", 599, 20);
+Producto producto2 = new Producto("Laptop Acer Nitro V", 301, "Tecnologia", 22000, 2);
+
+//Llamado de productos
+producto.Informacion();
+Console.WriteLine();
+producto.PrecioConDescuento(200);
+producto.ValorTotalDelInventario();
+producto.PreciodelEnvio(350);
+producto.PrecioconImpuesto(300);
+Console.WriteLine();
+producto1.Informacion();
+Console.WriteLine();
+producto1.PrecioConDescuento(150);
+producto1.ValorTotalDelInventario();
+producto1.PreciodelEnvio(250);
+producto1.PrecioconImpuesto(100);
+Console.WriteLine();
+producto2.Informacion();
+Console.WriteLine();
+producto2.PrecioConDescuento(1150);
+producto2.ValorTotalDelInventario();
+producto2.PreciodelEnvio(500);
+producto2.PrecioconImpuesto(2150);
